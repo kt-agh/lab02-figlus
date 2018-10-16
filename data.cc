11987825239eeb438e00bc727fe017cfd3339913
Rafal <rafal.figlus99@gmail.com>
2018-10-16 09:30:21 -0700

    data.cc

---
+++ data.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a,b;        
+cin>>a;
+cin>>b;
+
+
+cout<<"Hello fucking world";
+cout<<"Wynik dodawania jest rowny: "<<a+b<<endl;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+return 0;
+}       
+
